037a52e01c0f78c424a72f213829e405d2d2ed23
huangyh <hyh.icedcoffee@foxmail.com>
2019-01-08 14:59:22 +0800

    git init

---
+++ pages/detail/detail.js
@@ -0,0 +1,135 @@
+// pages/detail/detail.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        task: {
+            name: "",
+            desc: "",
+            num: 1,
+            id: -1
+        }
+    },
+
+    onChange(event) {
+        this.setData({
+            ["task.num"]: event.detail / 10
+        })
+    },
+
+    setName(e) {
+        this.setData({
+            ["task.name"]: e.detail
+        })
+    },
+
+    setDesc(e) {
+        this.setData({
+            ["task.desc"]: e.detail
+        })
+    },
+
+    back() {
+        wx.navigateTo({
+            url: "/pages/home/home"
+        })
+    },
+
+    /**
+     * 将任务存储
+     */
+    done() {
+        wx.navigateTo({
+            url: "/pages/home/home"
+        })
+        var curTask = this.data.task
+        var tasks = wx.getStorageSync("tasks") || []
+
+        // 新任务
+        if (curTask.id == -1) {
+            if (tasks.length == 0) {
+                curTask.id = 0;
+            } else {
+                curTask.id = tasks[0].id + 1
+            }
+            tasks.unshift(curTask)
+        }
+        // 编辑任务
+        else {
+            tasks[curTask.id] = curTask
+        }
+        
+        wx.setStorageSync("tasks", tasks)
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+        var id = options.id
+        if (id > -1) {
+            var tasks = wx.getStorageSync("tasks")
+            for (var task of tasks) {
+                if (task.id == id) {
+                    this.setData({
+                        task: task
+                    })
+                    break
+                }
+            }
+        }
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
+        wx.setNavigationBarTitle({
+            title: "任务编辑"
+        })
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {
+
+    }
+})
---
+++ pages/home/home.js
@@ -0,0 +1,84 @@
+// pages/home/home.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        taskCount: 0,
+        tasks: []
+    },
+
+    addTask(){
+        wx.navigateTo({
+            url: "/pages/detail/detail"
+        })
+    },
+
+    editTask(e){
+        wx.navigateTo({
+            url: `/pages/detail/detail?id=${e.target.id}`
+        })
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+        var tasks = wx.getStorageSync("tasks") || []
+        if(tasks.length > 0){
+            this.setData({
+                tasks: tasks
+            })
+        }
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {
+
+    }
+})
